0c8ea36b2dc8649cadb342169c32a03e0d684b41
Mohammed Nasim <mohammednasim2004@gmail.com>
2023-10-07 05:10:02 +0530

    Header and sidebar started.
    Just started with the header and sidebar. Installed bootstrap .

---
+++ app/src/App.tsx
@@ -0,0 +1,12 @@
+import SideBar from "./components/SideBar";
+import Header from "./components/Header"
+function App(){
+  return (
+    <div>
+      <Header />
+      <SideBar />
+    </div>
+  );
+}
+
+export default App;
